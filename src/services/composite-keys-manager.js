edd9832da71f4d8bb28218217fa922c0e000284e
Romain Gilliotte <romaing@forestadmin.com>
2021-04-23 12:22:31 +0200

    test: make composite-keys-manager tests pass

--- src/services/composite-keys-manager.js
+++ src/services/composite-keys-manager.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-import { Schemas } from 'forest-express';
 import Operators from '../utils/operators';
 
 /**
@@ -7,33 +6,38 @@ import Operators from '../utils/operators';
  * of using collection which have composite primary keys.
  */
 class CompositeKeysManager {
-  constructor(model, glue = '|') {
-    this._glue = glue;
-    this._model = model;
-    this._schema = Schemas.schemas[model.name];
-    this._Sequelize = this._model.sequelize.constructor;
+  static _GLUE = '|'
+
+  constructor(model) {
+    this._primaryKeys = _.keys(model.primaryKeys);
+    this._Sequelize = model.sequelize.constructor;
   }
 
   /** Build sequelize where condition from a list of packed recordIds */
   getRecordsConditions(recordIds) {
     const Ops = Operators.getInstance({ Sequelize: this._Sequelize });
-    if (recordIds.length === 0) {
-      throw new Error('At least one recordId must be provided');
+    if (this._primaryKeys.length === 0) {
+      throw new Error('No primary key was found');
     }
 
-    // If the ids are not composite, use a IN.
-    if (!this._schema.isCompositePrimary) {
-      return { [this._schema.idField]: recordIds };
+    if (recordIds.length <= 0) {
+      return this._Sequelize.literal('(0=1)');
+    }
+
+    if (this._primaryKeys.length === 1) {
+      return recordIds.length === 1
+        ? { [this._primaryKeys[0]]: recordIds[0] }
+        : { [this._primaryKeys[0]]: recordIds };
     }
 
     return recordIds.length === 1
       ? this._getRecordConditions(recordIds[0])
-      : { [Ops.OR]: recordIds.map((recordId) => this._getRecordConditions(recordId)) };
+      : { [Ops.OR]: recordIds.map((id) => this._getRecordConditions(id)) };
   }
 
   /* Annotate records with their packed primary key */
   annotateRecords(records) {
-    if (this._schema.isCompositePrimary) {
+    if (this._primaryKeys.length > 1) {
       records.forEach((record) => {
         record.forestCompositePrimary = this._createCompositePrimary(record);
       });
@@ -42,48 +46,29 @@ class CompositeKeysManager {
 
   /** Build sequelize where condition from a single packed recordId */
   _getRecordConditions(recordId) {
-    const primaryKeyValues = this._getPrimaryKeyValues(recordId);
-
-    // Invalid id => return no records.
-    if (primaryKeyValues.length !== _.keys(this._model.primaryKeys).length) {
-      return this._Sequelize.literal('(0=1)');
-    }
-
-    const where = {};
-    _.keys(this._model.primaryKeys).forEach((primaryKey, index) => {
-      where[primaryKey] = primaryKeyValues[index];
-    });
-
-    return where;
+    return _.zipObject(this._primaryKeys, this._getPrimaryKeyValues(recordId));
   }
 
   /** Create packed recordId from record */
   _createCompositePrimary(record) {
-    let compositePrimary = '';
-
-    _.keys(this._model.primaryKeys).forEach((primaryKey, index) => {
-      // Prevent liana to crash when a composite primary keys is null,
-      // this behaviour should be avoid instead of fixed.
-      if (record[primaryKey] === null) {
-        record[primaryKey] = 'null';
-      }
-      if (index === 0) {
-        compositePrimary = record[primaryKey];
-      } else {
-        compositePrimary = compositePrimary + this._glue + record[primaryKey];
-      }
-    });
-
-    return compositePrimary;
+    return this._primaryKeys.map(
+      (field) => (record[field] === null ? 'null' : record[field]),
+    ).join(CompositeKeysManager._GLUE);
   }
 
   /** Unpack recordId into an array */
   _getPrimaryKeyValues(recordId) {
     // Prevent liana to crash when a composite primary keys is null,
     // this behaviour should be avoid instead of fixed.
-    return recordId
-      .split(this._glue)
+    const unpacked = recordId
+      .split(CompositeKeysManager._GLUE)
       .map((key) => (key === 'null' ? null : key));
+
+    if (unpacked.length !== this._primaryKeys.length) {
+      throw new Error('Invalid packed primary key');
+    }
+
+    return unpacked;
   }
 }
 
